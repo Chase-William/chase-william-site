d43209c6e143c9e3f18366a2b041d838daf88d9e
cclcr21 <cclcr21@rit.edu>
2022-02-24 17:55:11 -0500

    updated .gitignore for netlify prod bundle

--- .netlify/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const entry = 'pages/index.svelte-631e68b5.js';
-export const js = ["pages/index.svelte-631e68b5.js","chunks/vendor-5cb3b40e.js"];
-export const css = ["assets/pages/index.svelte-18253608.css"];
